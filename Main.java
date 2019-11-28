f8bab861fe5f91a403352d123d3604bfb756792e
Pierre Gimalac <pierre.gimalac@gmail.com>
2019-11-28 16:36:09 +0100

    main

--- Main.java
+++ Main.java
@@ -92,7 +92,8 @@ public class Main {
                 .voisins[Integer.parseInt(line[2])] = Integer.parseInt(line[4]);
         }
 
-        stupide();
+        bnb();
+        //stupide();
     }
 
     public static void stupide() {
@@ -105,4 +106,146 @@ public class Main {
         System.out.println("");
     }
 
+
+
+
+
+
+
+    /*
+        SYLVAIN
+    */
+
+    static int final_res = 999999999;
+    static int INT_MAX   = 999999999;
+    static int N = 4;
+    static int[] final_path = new int[N+1];
+    static boolean[] visited = new boolean[N+1];
+
+
+    //Branch and bound algorithm
+    public static void bnb() {
+        for(int i = 0; i<final_path.length; i++){
+            final_path[i] = -1;
+        }
+        //final_path=[-1 for i in range(N+1)]
+        visited[0]=true;
+        for(int i=0;i<visited.length;i++){
+            visited[i]=false;
+        }
+
+        TSP();
+    }
+
+    public static void copyToFinal(int[] curr_path) {
+        for(int i=0;i<N;i++){
+            final_path[i] = curr_path[i] ;
+
+        }
+        final_path[N] = curr_path[0];
+    }
+
+
+
+
+    public static int firstMin(int i) {
+        int mini = INT_MAX ;
+        for(int k=0;k<N;k++){if (Main.fournisseurs[i].voisins[k]<mini && i != k) {
+                mini = Main.fournisseurs[i].voisins[k] ;
+            }
+        }
+        return mini ;
+    }
+
+
+    public static int secondMin(int i) {
+
+        int first = INT_MAX;
+        int second = INT_MAX;
+        for(int j=0;j<N;j++) {
+            if (i == j) {
+                continue ;
+            }
+
+            if (Main.fournisseurs[i].voisins[j] <= first) {
+                second = first ;
+                first = Main.fournisseurs[i].voisins[j] ;
+            }
+            else if ((Main.fournisseurs[i].voisins[j] <= second) &&  (Main.fournisseurs[i].voisins[j] != first)) {
+                second = Main.fournisseurs[i].voisins[j];
+            }
+        }
+        return second;
+    }
+
+    public static void TSPRec(int curr_bound, int curr_weight, int level, int[] curr_path) {
+
+        if (level==N)  {
+
+            if (Main.fournisseurs[curr_path[level-1]].voisins[curr_path[0]] != 0)  {
+                int curr_res = curr_weight + Main.fournisseurs[curr_path[level-1]].voisins[curr_path[0]];
+                if (curr_res < final_res) {
+                    copyToFinal(curr_path);
+                    final_res = curr_res;
+                }
+            }
+        }
+
+        for (int i=0;i<N;i++) {
+            if (Main.fournisseurs[curr_path[level-1]].voisins[i] != 0 && !visited[i]) {
+                int temp = curr_bound;
+                curr_weight += Main.fournisseurs[curr_path[level-1]].voisins[i];
+
+                if (level==1) {
+                  curr_bound -= ((firstMin(curr_path[level-1]) + firstMin(i))/2);
+                }
+                else {
+                  curr_bound -= ((secondMin(curr_path[level-1]) + firstMin(i))/2);
+                }
+
+                if (curr_bound + curr_weight < final_res)  {
+                    curr_path[level] = i;
+                    visited[i] = true;
+
+                    TSPRec(curr_bound, curr_weight, level+1,curr_path);
+                }
+
+                curr_weight -= Main.fournisseurs[curr_path[level-1]].voisins[i];
+                curr_bound = temp;
+
+                for (int j=0;j<N+1;j++){
+                    visited[j]=false;
+                }
+
+                for(int j=0;j<level;j++){
+                    visited[curr_path[j]] = true;
+                }
+            }
+        }
+    }
+
+
+    public static void TSP()  {
+        int curr_bound= 0;
+        int[] curr_path = new int[N+1];
+        for(int i=0;i<curr_path.length;i++){
+            curr_path[i] = -1;
+        }
+
+        int[] visited = new int[N+1];
+        for(int i=0;i<visited.length;i++){
+            visited[i]=0;
+        }
+
+        for(int i=0;i<N;i++){
+            curr_bound += (firstMin(i) + secondMin(i));
+        }
+
+        curr_bound = curr_bound/2 + 1; //arrondir correctement
+
+        Main.visited[0] = true ;
+        curr_path[0] = 0;
+
+        TSPRec(curr_bound, 0, 1, curr_path);
+    }
 }
